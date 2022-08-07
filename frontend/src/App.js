a04507dc2aa02481149cf5caf158b8b02a63a626
Oleksandr Savenkov <savenkovalx@gmail.com>
2022-08-07 22:36:47 +0300

    add github login submission form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,66 @@
+import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 
 export default function App() {
   return (
       <div>
-          <DisplayRepos />
+          <LoginForm />
       </div>
   );
 }
 
 const GET_REPOS= gql`
-  query{
-  github(login: "SavenkovAlx") {
+  query github($login : String!) {
+  github(login: $login){
     username
     repos
   }
 }
 `;
 
-function DisplayRepos() {
-    const { loading, error, data } = useQuery(GET_REPOS);
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: '', submited: false};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.setState({submit: true});
+    }
+
+    render() {
+        if (this.state.submit) {
+            return (
+            <DisplayRepos login={this.state.value } />
+
+        );
+        } else {
+            return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Enter github login:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="send" />
+            </form>
+        );}
+
+    }
+}
+
+function DisplayRepos(login) {
+    const { loading, error, data } = useQuery(GET_REPOS, {
+        variables: {
+            login: login["login"]
+        }});
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>`Error ${error.message}`</p>;
@@ -31,6 +73,7 @@ function DisplayRepos() {
                     return <li key={index}>{item}</li>;
                 })}
             </ul>
+            <button onClick={() => { window.location.reload()}}>Refresh</button>
         </div>
     );
 }
