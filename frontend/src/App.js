5a2fef5e49915f5e9c131a054544983e50a2d531
Oleksandr Savenkov <savenkovalx@gmail.com>
2022-08-07 20:19:14 +0300

    getting data from api by static login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,34 @@ import { useQuery, gql } from '@apollo/client';
 export default function App() {
   return (
       <div>
-        <h2>My first Apollo app 🚀</h2>
+          <DisplayRepos />
       </div>
   );
+}
+
+const GET_REPOS= gql`
+  query{
+  github(login: "SavenkovAlx") {
+    username
+    repos
+  }
+}
+`;
+
+function DisplayRepos() {
+    const { loading, error, data } = useQuery(GET_REPOS);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>`Error ${error.message}`</p>;
+
+    return (
+        <div className="container">
+            <h3>{data.github.username}</h3>
+            <ul>
+                {data.github.repos.map((item, index) => {
+                    return <li key={index}>{item}</li>;
+                })}
+            </ul>
+        </div>
+    );
 }
