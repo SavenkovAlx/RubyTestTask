c92b1bbe6528d51f322305a5f479c3ee1cafea67
Oleksandr Savenkov <savenkovalx@gmail.com>
2022-08-10 19:45:24 +0300

    changed save path for cassettes

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -5,7 +5,7 @@ VCR.configure do |c|
   c.default_cassette_options = {
     decode_compressed_response: true
   }
-  c.cassette_library_dir = 'vcr/cassettes'
+  c.cassette_library_dir = 'spec/vcr/cassettes'
   c.allow_http_connections_when_no_cassette = true
 end
 
